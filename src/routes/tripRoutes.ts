d90e7d62032821a155e82f0e42e8f141299f98ee
kharayulcha <bayungraiprerna@gmail.com>
2026-02-23 11:45:11 +0545

    US-13 implement start trip

--- src/routes/tripRoutes.ts
+++ src/routes/tripRoutes.ts
@@ -1,10 +1,14 @@
 import express from 'express';
-import { endTrip, startTrip } from '../controller/tripController.js';
+import { endTrip, getActiveTrip, getMyActiveTrip, getTripLocations, saveLocation, startTrip } from '../controller/tripController.js';
 import catchAsync from '../utils/catchAsync.js';
 
 const router = express.Router();
 
 router.post('/start', catchAsync(startTrip));
 router.post('/end', catchAsync(endTrip));
+router.post('/location', catchAsync(saveLocation));
+router.get('/my-active', catchAsync(getMyActiveTrip));
+router.get('/active/:routeId', catchAsync(getActiveTrip));
+router.get('/locations/:tripId', catchAsync(getTripLocations));
 
 export default router;
